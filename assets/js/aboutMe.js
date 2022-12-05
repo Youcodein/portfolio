889edb4999d29051ef63483d0d6befd1c3e7938a
HWANGYoujin <ghkddbwls96@gmail.com>
2022-12-06 08:04:34 +0900

    123

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -1,17 +1,27 @@
 
 //모달
-const modalBtn = document.querySelectorAll(".modalBtn");
+const modalBtn = document.querySelector(".modalBtn");
 const modalCont = document.querySelector(".modal__cont");
 const modalClose = document.querySelector(".modal__close");
-
 
 
 modalBtn.addEventListener("click", () => {
     modalCont.classList.add("show");
     modalCont.classList.remove("hide");
 })
+
 modalClose.addEventListener("click",()=>{
     modalCont.classList.add("hide");
 
 })
+
+
+// modalBtn.addEventListener("click", () => {
+//     modalCont.classList.add("show");
+//     modalCont.classList.remove("hide");
+// })
+// modalClose.addEventListener("click",()=>{
+//     modalCont.classList.add("hide");
+
+// })
 
