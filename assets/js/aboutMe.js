32fc453f4cc95d7db62a69c9bd41363c02fd828b
HWANGYoujin <ghkddbwls96@gmail.com>
2022-12-08 07:38:48 +0900

    123

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -1,20 +1,34 @@
 
 //모달
-const modalBtn = document.querySelector(".modalBtn");
-const modalCont = document.querySelector(".modal__cont");
-const modalClose = document.querySelector(".modal__close");
+var modalBtn = document.querySelector(".modalBtn");
+var modalCont = document.querySelector(".modal__cont");
+var modalClose = document.querySelector(".modal__close");
+var funcs = [];
 
 
-modalBtn.addEventListener("click", () => {
-    modalCont.classList.add("show");
-    modalCont.classList.remove("hide");
-})
 
-modalClose.addEventListener("click",()=>{
-    modalCont.classList.add("hide");
 
-})
+function Modal(num) {
+    return function () {
+        modalBtn.addEventListener("click", () => {
+            modalCont.classList.add("show");
+            modalCont.classList.remove("hide");
+        })
+        
+        modalClose.addEventListener("click",()=>{
+            modalCont.classList.add("hide");
+        
+        })
+    };
+}
 
+for (var i = 0; i < modalBtn.length; i++) {
+    funcs[i] = Modal(i);
+}
+
+for(var j = 0; j < modalBtn.length; j++) {
+    funcs[j]();
+  }
 
 // modalBtn.addEventListener("click", () => {
 //     modalCont.classList.add("show");
