987c496978d33a059e612f48cc63314977165e6b
Youcodein <ghkddbwls96@gmail.com>
2022-12-07 10:57:13 +0900

    123

---
+++ assets/js/musicPlayer.js
@@ -0,0 +1,26 @@
+const allMusic = [
+    {
+        name: "1. Campfire",
+        artist: "Telecasted",
+        img: "music__img1",
+        audio: "music_audio01"
+    },
+    {
+        name: "1. Campfire",
+        artist: "Telecasted",
+        img: "music__img1",
+        audio: "music_audio01"
+    },
+    {
+        name: "1. Campfire",
+        artist: "Telecasted",
+        img: "music__img1",
+        audio: "music_audio01"
+    },
+    {
+        name: "1. Campfire",
+        artist: "Telecasted",
+        img: "music__img1",
+        audio: "music_audio01"
+    }
+]
