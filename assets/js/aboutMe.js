688eb31c7da71e92fc440104dcf410c46d78ed8d
Youcodein <ghkddbwls96@gmail.com>
2022-12-08 09:22:09 +0900

    123

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -3,39 +3,39 @@
 var modalBtn = document.querySelector(".modalBtn");
 var modalCont = document.querySelector(".modal__cont");
 var modalClose = document.querySelector(".modal__close");
-var funcs = [];
+// var funcs = [];
 
 
 
 
-function Modal(num) {
-    return function () {
-        modalBtn.addEventListener("click", () => {
-            modalCont.classList.add("show");
-            modalCont.classList.remove("hide");
-        })
+// function Modal(num) {
+//     return function () {
+//         modalBtn.addEventListener("click", () => {
+//             modalCont.classList.add("show");
+//             modalCont.classList.remove("hide");
+//         })
         
-        modalClose.addEventListener("click",()=>{
-            modalCont.classList.add("hide");
+//         modalClose.addEventListener("click",()=>{
+//             modalCont.classList.add("hide");
         
-        })
-    };
-}
+//         })
+//     };
+// }
 
-for (var i = 0; i < modalBtn.length; i++) {
-    funcs[i] = Modal(i);
-}
+// for (var i = 0; i < modalBtn.length; i++) {
+//     funcs[i] = Modal(i);
+// }
 
-for(var j = 0; j < modalBtn.length; j++) {
-    funcs[j]();
-  }
+// for(var j = 0; j < modalBtn.length; j++) {
+//     funcs[j]();
+//   }
 
-// modalBtn.addEventListener("click", () => {
-//     modalCont.classList.add("show");
-//     modalCont.classList.remove("hide");
-// })
-// modalClose.addEventListener("click",()=>{
-//     modalCont.classList.add("hide");
+modalBtn.addEventListener("click", () => {
+    modalCont.classList.add("show");
+    modalCont.classList.remove("hide");
+})
+modalClose.addEventListener("click",()=>{
+    modalCont.classList.add("hide");
 
-// })
+})
 
