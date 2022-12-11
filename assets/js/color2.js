0afce183f87c2546821b068c726039e56ef70c9a
HWANGYoujin <ghkddbwls96@gmail.com>
2022-12-12 06:37:15 +0900

    123

---
+++ assets/js/color2.js
@@ -0,0 +1,197 @@
+const decoBtn = document.querySelectorAll(".decoBtn button");
+
+// mainColor
+
+// leftDisplay.style.backgroundColor = color1;
+        // RightDisplaySidebar.style.backgroundColor = color1;
+        // RightnDisplayMusicPlayer.style.backgroundColor = color1;
+        // RightnDisplayDecoration.style.backgroundColor = color1;
+        // RightDisplayGame.style.backgroundColor = color1;
+        // centerDisplayMonitor.style.borderColor = color1;
+        // centerDisplayTopBtnBg.forEach((ea) => {
+        //     ea.style.backgroundColor = color1;
+        // });
+        // centerDisplayMiddleTitle.forEach((eb) => {
+        //     eb.style.backgroundColor = color1;
+        // });
+//         const centerDisplayTopBtnBg = document.querySelectorAll(".centerDisplay__top .topBtn");
+// const centerDisplayMiddleTitle = document.querySelectorAll(".centerDisplay__middle__title");
+
+const mainColor = document.querySelectorAll(".mainColor")
+const mainColorFont = document.querySelectorAll(".mainColorFont")
+
+
+// const leftDisplay = document.querySelector(".leftDisplay");
+
+// const RightDisplaySidebar = document.querySelector(".rigthDisplay__sidebar");
+// const RightnDisplayMusicPlayer = document.querySelector(".rigthDisplay__musicplayer");
+// const RightnDisplayDecoration = document.querySelector(".rigthDisplay__decoration");
+// const RightDisplayGame = document.querySelector(".rigthDisplay__game");
+const centerDisplayMonitor = document.querySelector(".centerDisplay__middle__monitor");
+// const leftDisplayMonitor = document.querySelector(".leftDisplay__monitor");
+
+// subColor
+const subColor = document.querySelectorAll(".subColor")
+const subColorFont = document.querySelectorAll(".subColorFont")
+
+const fontColorOp = document.querySelectorAll(".fontColorOp")
+
+// const centerDisplayTop = document.querySelector(".centerDisplay__top");
+// const centerDisplayMiddle = document.querySelector(".centerDisplay__middle");
+// const centerDisplayMBottomSNS = document.querySelector(".centerDisplay__bottom__SNS");
+// const centerDisplayMBottomContactMe = document.querySelector(".centerDisplay__bottom__ContactMe");
+// const RightDisplay = document.querySelector(".rightDisplay");
+
+
+// 박스섀도우 버튼
+
+const squareShadow1 = document.querySelector(".aboutMeBtn");
+const squareShadow2 = document.querySelector(".gameStartBtn");
+
+// btnBg
+const snsBtnBg = document.querySelectorAll(".snsBtn");
+
+// Mainbg
+const mainBG = document.querySelector("body");
+
+// dot
+const dot = document.querySelectorAll(".dot");
+
+// line
+const line1 = document.querySelector(".line");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
+
+// font
+const headerTitle = document.querySelector(".leftDisplay__header");
+const modeChangeTitle = document.querySelector(".modeChange__title h2");
+
+let colorList = [
+    "btn1",
+    "btn2",
+    "btn3",
+    "btn4"
+];
+
+let main = [
+    "var(--main-color1)",
+    "var(--main-color2)",
+    "var(--main-color3)",
+    "var(--main-color4)",
+];
+let sub = [
+    "var(--sub-color1)",
+    "var(--sub-color2)",
+    "var(--sub-color3)",
+    "var(--sub-color4)",
+];
+
+
+
+function whatColor(click) {
+    console.log(click)
+    switch (click) {
+        case "deco1":
+            colorChage1("var(--main-color1)");
+            colorChage2("var(--sub-color1)");
+            colorChage3("var(--bg-color1)");
+            colorChage4("var(--fontColorOp1)");
+            colorChage5("var(--boxShadow1)");
+            break;
+        case "deco2":
+            colorChage1("var(--main-color2)");
+            colorChage2("var(--sub-color2)");
+            colorChage3("var(--bg-color2)");
+            colorChage4("var(--fontColorOp2)");
+            colorChage5("var(--boxShadow2)");
+            break;
+        case "deco3":
+            colorChage1("var(--main-color3)");
+            colorChage2("var(--sub-color3)");
+            colorChage3("var(--bg-color3)");
+            colorChage4("var(--fontColorOp3)");
+            colorChage5("var(--boxShadow3)");
+            break;
+        case "deco4":
+            colorChage1("var(--main-color4)");
+            colorChage2("var(--sub-color4)");
+            colorChage3("var(--bg-color4)");
+            colorChage4("var(--fontColorOp4)");
+            colorChage5("var(--boxShadow4)");
+            break;
+    }
+}
+
+function colorChage1(color1) {
+        // leftDisplay.style.backgroundColor = color1;
+        // RightDisplaySidebar.style.backgroundColor = color1;
+        // RightnDisplayMusicPlayer.style.backgroundColor = color1;
+        // RightnDisplayDecoration.style.backgroundColor = color1;
+        // RightDisplayGame.style.backgroundColor = color1;
+        centerDisplayMonitor.style.borderColor = color1;
+        // centerDisplayTopBtnBg.forEach((ea) => {
+        //     ea.style.backgroundColor = color1;
+        // });
+        // centerDisplayMiddleTitle.forEach((eb) => {
+        //     eb.style.backgroundColor = color1;
+        // });
+        mainColor.forEach((em) => {
+            em.style.backgroundColor = color1;
+        });
+        mainColorFont.forEach((emf) => {
+            emf.style.color = color1;
+        });
+        
+}
+
+function colorChage2(color2) {
+    // centerDisplayTop.style.backgroundColor = color2;
+    // centerDisplayMiddle.style.backgroundColor = color2;
+    // centerDisplayMBottomSNS.style.backgroundColor = color2;
+    // centerDisplayMBottomContactMe.style.backgroundColor = color2;
+    // RightDisplay.style.backgroundColor = color2;
+    // leftDisplayMonitor.style.borderColor = color2;
+    subColor.forEach((es) => {
+        es.style.backgroundColor = color2;
+    });
+    subColorFont.forEach((esf) => {
+        esf.style.color = color2;
+    });
+    dot.forEach((ed) => {
+        ed.style.backgroundColor = color2;
+    });
+    
+    line1.style.backgroundColor = color2;
+
+    line2.style.backgroundColor = color2;
+
+    line3.style.backgroundColor = color2;
+    // headerTitle.style.color = color2;
+    // modeChangeTitle.style.color = color2;
+
+}
+
+function colorChage3(color3) {
+    mainBG.style.backgroundColor = color3;
+}
+
+function colorChage4(color4) {
+    fontColorOp.forEach((efo)=>{
+        efo.style.color = color4;
+    })
+
+}
+function colorChage5(color5) {
+    squareShadow1.style.boxShadow = inherit | inherit | inherit | inherit | color5;
+
+
+}
+
+
+
+decoBtn.forEach((e) => {
+    e.addEventListener("click", () => {
+        // console.log(e.value)
+        whatColor(e.value);
+    });
+});
--- assets/js/musicPlayer.js
+++ assets/js/musicPlayer.js
@@ -22,11 +22,83 @@ const allMusic = [
 ]
 
 const musicWrap = document.querySelector(".rigthDisplay__musicplayer");
+const musicName = musicWrap.querySelector(".music__view__title .title");
+const musicArtist = musicWrap.querySelector(".music__view__title .artist");
+const musicAudio = musicWrap.querySelector("#main-audio");
+const musicPlay = musicWrap.querySelector("#control-play");
+
+const musicProgress = musicWrap.querySelector(".progress");
+const musicProgressBar = musicWrap.querySelector(".progress .bar");
+const musicProgressCurrent = musicWrap.querySelector(".progress .timer .current");
+const musicProgressDuration = musicWrap.querySelector(".progress .timer .duration");
+
+let musicIndex = 1;                                                 //현재 음악 인덱스
+
 
 // 음악 재생
 
 function loadMusic(num){
     musicName.innerText = allMusic[num-1].name;                     //뮤직 이름 로드
     musicArtist.innerText = allMusic[num-1].artist;                 //뮤직 아티스트 로드
-    musicAudio.src=`../assets/audio/${allMusic[num-1] .audio}.mp3`  //뮤직 로드
+    musicAudio.src=`../music/${allMusic[num-1] .audio}.mp3`  //뮤직 로드
 }
+
+// 재생버튼
+function playMusic(){
+    musicWrap.classList.add("paused");
+    musicPlay.setAttribute("title","정지");
+    musicPlay.setAttribute("class","stop");
+    musicAudio.play();
+}
+
+// 정지버튼
+function pauseMusic(){
+    musicWrap.classList.remove("paused");
+    musicAudio.pause();
+    musicPlay.setAttribute("title","재생");
+    musicPlay.setAttribute("class","play");
+}
+
+// 뮤직 진행바
+musicAudio.addEventListener("timeupdate", e => {
+    // console.log(e);
+    const currentTime = e.target.currentTime;                           //오디오의 현재 재생되는 시간
+    const duration = e.target.duration;                                 //오디오의 총 길이
+    let progressWidth = (currentTime/duration) * 100;                   //전체 깅이에서 현재 진행되는 시간을 백분위로 나눠줌
+
+    musicProgressBar.style.width = `${progressWidth}%`;
+
+    //전체 시간
+    musicAudio.addEventListener("loadeddata", ()=>{
+        let audioDuration = musicAudio.duration;
+        let totalMin = Math.floor(audioDuration / 60);                  //전체 시간(초)을 분단위로 쪼갬
+        let totalSec = Math.floor(audioDuration % 60);                  //남은 초를 저장
+
+        if(totalSec < 10) totalSec = `0${totalSec}`;                    //초가 한 자리 수 일때 10의 자리에 0을 붙임
+        musicProgressDuration.innerText = `${totalMin}:${totalSec}`;    //완성된 시간 문자열
+    })
+
+    //진행 시간
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if(currentSec<10) currentSec = `0${currentSec}`;
+    musicProgressCurrent.innerText = `${currentMin}:${currentSec}`;
+
+});
+//진행 버튼 클릭
+musicProgress.addEventListener("click", (e) => {
+    let progressWidth =musicProgress.clientWidth;                                   //진행바 전체 길이
+    let clickedOffsetX = e.offsetX;                                                 //진행바 기준으로 측정되는 X좌표값
+    let songDuration = musicAudio.duration;                                         //오디오 전체 길이
+
+    musicAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration        //백분위로 나눈 숫자에 다시 전체 길이를 곱해서 현재 재생값으로 바꿈
+})
+
+//플레이 버튼 클릭
+musicPlay.addEventListener("click", ()=>{
+    const isMusicPaused = musicWrap.classList.contains("paused");   //음악이 재생되고 있다는 뜻
+    isMusicPaused ? pauseMusic() : playMusic();
+
+})
+
+
