bdc06114c122b208b4d7c3c6490868aedb7a04a0
Youcodein <ghkddbwls96@gmail.com>
2022-12-12 11:18:26 +0900

    123

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,3 +7,10 @@ gameStartBtn.addEventListener("click", () => {
     myWork.classList.remove("show");
     game.classList.add("show");
 })
+
+const goBackBtn = document.querySelector(".goBack");
+
+goBackBtn.addEventListener("click", () => {
+    myWork.classList.add("show");
+    game.classList.remove("show");
+})
