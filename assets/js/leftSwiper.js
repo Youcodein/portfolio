0dcf293e1c0389a11136f08a1615f066e0ae4fd0
Youcodein <ghkddbwls96@gmail.com>
2022-12-13 10:42:28 +0900

    123

---
+++ assets/js/leftSwiper.js
@@ -0,0 +1,17 @@
+// const likeBirdBtn = document.querySelector(".likeBtn .btn1");
+// const likeTeaBtn = document.querySelector(".likeBtn .btn2");
+// const likeBreadBtn = document.querySelector(".likeBtn .btn3");
+// const likeDrawBtn = document.querySelector(".likeBtn .btn4");
+// const likeBookBtn = document.querySelector(".likeBtn .btn5");
+
+
+// const mainImg = document.querySelector(".mainImg");
+// const birdImg = document.querySelector(".birdImg");
+// const teaImg = document.querySelector(".teaImg");
+// const drawImg = document.querySelector(".drawImg");
+// const bookImg = document.querySelector(".bookImg");
+
+// likeBirdBtn.addEventListener("click", () => {
+//     mainImg.classList.remove("show");
+    
+// })
